3ee403a010159f6adaf127782ab2fede673044c3
Mariana Melo <110180991+Marianadsm@users.noreply.github.com>
2024-09-29 22:58:25 -0300

    initial commit

---
+++ discount-calculation-api/src/main/java/br/com/igen/discount_calculation_api/DiscountCalculationApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.igen.discount_calculation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountCalculationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountCalculationApiApplication.class, args);
+	}
+
+}
---
+++ discount-calculation-api/src/test/java/br/com/igen/discount_calculation_api/DiscountCalculationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igen.discount_calculation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountCalculationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
